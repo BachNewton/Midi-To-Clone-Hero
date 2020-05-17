519bb3effd8a0f03744e4f95e3d79ec921f687d7
BachNewton <kyle1235@gmail.com>
2020-05-17 12:19:20 -0400

    Simplified boolean expression

--- scripts/NoteEvent.py
+++ scripts/NoteEvent.py
@@ -2,7 +2,7 @@ class NoteEvent:
     def __init__(self, time, pitch, velocity, channel):
         self.time = int(time)
         self.pitch = int(pitch)
-        self.isOnEvent = False if velocity == '0' else True
+        self.isOnEvent = velocity != '0'
         self.channel = int(channel) - 1
 
     def get_chart_line(self, time_scale, duration):
